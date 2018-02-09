8dfededd1080717103e609fbfc5f4f9149073c38
davidobrien1 <36315455+davidobrien1@users.noreply.github.com>
2018-02-09 23:48:51 +0000

    Create fibname.py
    Semester 1 Programming and Scripting - Exercise 1 and 2 combined

---
+++ fibname.py
@@ -0,0 +1,27 @@
+# Ian McLoughlin
+# A program that displays Fibonacci numbers using people's names.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "OBrien"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
