febf4e8fa43bf4af6ce024bc113e65e4eac48075
O'Brien <dobrien@coffeygroup.com>
2018-03-02 21:22:24 +0000

    final format.py file

--- format.py
+++ format.py
@@ -3,4 +3,5 @@
 # examples from 7.1 of python tutorial
 
 for i in range(1,11): # the for statement loops through each of the numbers from 1 to 10
-  print(i, i**2, i**3, i**4) # prints out i, then the square of i, then the cube of i. Putting in 2 no. ** implies "to the power of.."
+  print('{:2d} {:3d} {:4d} {:5d}'.format(i, i**2, i**3, i**4)) # this format notation allows
+ 
